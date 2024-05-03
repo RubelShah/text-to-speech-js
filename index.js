0e6fb1a39b060857024ac960b4d62b4dd4f8998b
RubelShah <rubelazharulislam@gmail.com>
2024-05-03 11:18:54 +0600

    cotrolling-speed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const playBtn = document.getElementById("play");
 const pauseBtn = document.getElementById("pause");
 const stopBtn = document.getElementById("stop");
 
+let currentcharacter;
 
 const utterance = new SpeechSynthesisUtterance();
 playBtn.addEventListener('click',()=>{
@@ -13,9 +14,17 @@ playBtn.addEventListener('click',()=>{
 utterance.addEventListener('end',()=>{
     textInput.disabled = false;
 });
+utterance.addEventListener('boundray',(e)=>{
+    currentcharacter = e.chartIndex;
+});
 pauseBtn.addEventListener('click',pause);
 
-stopBtn.addEventListener('click',stopUtterance)
+stopBtn.addEventListener('click',stopUtterance);
+
+speedInput.addEventListener('input',()=>{
+    stopUtterance();
+    playtext(utterance.text.substring(currentcharacter))
+});
 
 function playtext(txt){
     if(speechSynthesis.paused && speechSynthesis.speaking){
